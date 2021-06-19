fc16a65e6517812af51c80bc09147f592f4d8326
RiFenn <86132003+RiFenn@users.noreply.github.com>
2021-06-19 14:20:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var FAQ = document.querySelector('#FAQ1');
+
+FAQ.addEventListener('mouseenter', function () {
+    FAQ.classList.add('heaver');
+});
+FAQ.addEventListener('mouseleave', function () {
+    FAQ.classList.remove('heaver');
+});
+
+var cre = document.querySelector('#creator1');
+
+cre.addEventListener('mouseenter', function () {
+    cre.classList.add('heaver');
+});
+cre.addEventListener('mouseleave', function () {
+    cre.classList.remove('heaver');
+});
+
+var cyb = document.querySelector('#cybersport1');
+
+cyb.addEventListener('mouseenter', function () {
+    cyb.classList.add('heaver');
+});
+cyb.addEventListener('mouseleave', function () {
+    cyb.classList.remove('heaver');
+});
+
+var com = document.querySelector('#community1');
+
+com.addEventListener('mouseenter', function () {
+    com.classList.add('heaver');
+});
+com.addEventListener('mouseleave', function () {
+    com.classList.remove('heaver');
+});
+
